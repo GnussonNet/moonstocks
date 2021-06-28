2bff5989c6541f9fb3c1dfc731a9f01a24a087d6
Gnusson.net <40320838+GnussonNet@users.noreply.github.com>
2021-06-28 21:11:53 +0200

    Removed search dropdown and added navbar logo

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,29 +3,33 @@ import { Plus, Star, Menu, Search } from 'react-feather';
 function App() {
   return (
     <nav className="navbar">
-      <h3 className="navbar-title">Moonstocks</h3>
-      <div className="navbar-search1">
-        <div className="navbar-search">
-          <div className="search">
-            <Search />
-            <input type="text" placeholder="Search for stock" />
-          </div>
-          <div className="search-results"></div>
+      {/* <h3 className="navbar-title">Moonstocks</h3> */}
+      <div className="navbar-logo-container">
+        <img className="navbar-logo" src="./img/logo.png" alt="Logo" />
+      </div>
+      <div className="navbar-search">
+        <div className="search">
+          <Search />
+          <input type="text" placeholder="Search for stock" />
         </div>
+        <div className="search-results"></div>
       </div>
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a href="add" className="icon-button">
+          {/* eslint-disable-next-line */}
+          <a className="icon-button">
             <Plus />
           </a>
         </li>
         <li className="nav-item">
-          <a href="favorites" className="icon-button">
+          {/* eslint-disable-next-line */}
+          <a className="icon-button">
             <Star />
           </a>
         </li>
         <li className="nav-item">
-          <a href="test" className="icon-button">
+          {/* eslint-disable-next-line */}
+          <a className="icon-button">
             <Menu />
           </a>
         </li>
