88b3806c2854dd9d64b7edd30e6de268cc547166
Gnusson.net <40320838+GnussonNet@users.noreply.github.com>
2021-07-03 08:55:08 +0200

    Responsive nav, menu and content area

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,66 +2,13 @@ import { Plus, Star, Menu, Search } from 'react-feather';
 import logo from '../../img/logo.png';
 
 function App() {
-
   function expandSearch() {
-    var element = document.querySelector(".navbar");
-    element.classList.toggle("expanded");
+    var element = document.querySelector('.navbar');
+    element.classList.toggle('expanded');
   }
 
-
-
   return (
-    // <>
-    //   <nav className="navbar">
-    //     <div className="navbar-logo-container">
-    //       <img className="navbar-logo" src={logo} alt="Logo" />
-    //     </div>
-    //     <div className="navbar-search">
-    //       <div className="search">
-    //         <Search />
-    //         <input type="text" placeholder="Search for stock" />
-    //       </div>
-    //       <div className="search-results"></div>
-    //     </div>
-    //     <ul className="navbar-nav">
-    //       <li className="nav-item">
-    //         {/* eslint-disable-next-line */}
-    //         <a onClick={expandSearch} className="icon-button">
-    //           <Search />
-    //         </a>
-    //       </li>
-    //       <li className="nav-item">
-    //         {/* eslint-disable-next-line */}
-    //         <a className="icon-button">
-    //           <Plus />
-    //         </a>
-    //       </li>
-    //       <li className="nav-item">
-    //         {/* eslint-disable-next-line */}
-    //         <a className="icon-button">
-    //           <Star />
-    //         </a>
-    //       </li>
-    //       <li className="nav-item">
-    //         {/* eslint-disable-next-line */}
-    //         <a className="icon-button">
-    //           <Menu />
-    //         </a>
-    //       </li>
-    //     </ul>
-    //   </nav>
-
-    //   <main>
-    //     <div className="container">
-    //       <div className="menu">
-
-    //       </div>
-    //       <div className="content"></div>
-    //     </div>
-    //   </main>
-    // </>
-
-    <>
+       <>
       <nav className="navbar">
         <div className="navbar-logo-container">
           <img className="navbar-logo" src={logo} alt="Logo" />
@@ -100,7 +47,14 @@ function App() {
           <div className="navbar-search-results"></div>
         </div>
       </nav>
-      <main></main>
+      <main>
+        <div className="main-container">
+          <div className="main-menu-container">
+            <div className="main-menu"></div>
+          </div>
+          <div className="main-content"></div>
+        </div>
+      </main>
     </>
   );
 }
