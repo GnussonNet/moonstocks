43def2945c71414955392bd78bbb44d61e903d80
Gnusson.net <admin@gnusson.net>
2021-07-06 22:33:31 +0200

    Added the menu for both small and large devices

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,28 @@
-import { Plus, Star, Menu, Search } from 'react-feather';
-import logo from '../../img/logo.png';
+import { Home, Briefcase, BarChart2, Clock, Bell, DollarSign, Settings, ChevronLeft, Plus, Star, Menu, Search } from 'react-feather';
+import Logo from '../../img/logo.png';
+import User from '../../img/user.jpeg';
 
 function App() {
-  function expandSearch() {
+  function toggleSearch() {
     var element = document.querySelector('.navbar');
     element.classList.toggle('expanded');
   }
 
+  function toggleMenu() {
+    var element = document.querySelector('.main-menu');
+    element.classList.toggle('expanded');
+  }
+
   return (
-       <>
+    <>
       <nav className="navbar">
         <div className="navbar-logo-container">
-          <img className="navbar-logo" src={logo} alt="Logo" />
+          <img className="navbar-logo" src={Logo} alt="Logo" />
         </div>
         <ul className="navbar-nav">
           <li className="navbar-nav-item button-search">
             {/* eslint-disable-next-line */}
-            <a onClick={expandSearch} className="navbar-icon-button">
+            <a onClick={toggleSearch} className="navbar-icon-button">
               <Search />
             </a>
           </li>
@@ -50,7 +56,56 @@ function App() {
       <main>
         <div className="main-container">
           <div className="main-menu-container">
-            <div className="main-menu"></div>
+            <div className="main-menu">
+              <div className="menu-account">
+                <div onClick={toggleMenu} className="menu-account-user">
+                  <div className="menu-account-user-name">
+                    <h5>Filip Magnusson</h5>
+                    <p>Premium user</p>
+                  </div>
+                  <img src={User} alt="Profile" />
+                  <ChevronLeft />
+                </div>
+              </div>
+
+              <hr />
+
+              <div className="menu-items">
+                <li className="menu-item">
+                  <Home />
+                  <p>Overview</p>
+                </li>
+                <li className="menu-item">
+                  <Briefcase />
+                  <p>Watchlists</p>
+                </li>
+                <li className="menu-item">
+                  <BarChart2 />
+                  <p>Stats</p>
+                </li>
+                <li className="menu-item">
+                  <Clock />
+                  <p>Time to Sell</p>
+                </li>
+                <li className="menu-item">
+                  <Bell />
+                  <p>Alerts</p>
+                </li>
+                <li className="menu-item">
+                  <DollarSign />
+                  <p>Sold Stocks</p>
+                </li>
+              </div>
+
+              <hr />
+
+              <div className="menu-settings">
+                <li className="menu-item">
+                  <Settings />
+                  <p>Settings</p>
+                </li>
+              </div>
+            </div>
           </div>
           <div className="main-content"></div>
         </div>
