4b8788907abc79516c8a8298839b7694d9aabfb2
Gnusson.net <admin@gnusson.net>
2021-07-07 19:47:44 +0200

    Added content to Overview page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Home, Briefcase, BarChart2, Clock, Bell, DollarSign, Settings, ChevronLeft, Plus, Star, Menu, Search } from 'react-feather';
+import { Home, Briefcase, BarChart2, Clock, Bell, DollarSign, Settings, ChevronLeft, Plus, Star, Menu, Search, ArrowUp } from 'react-feather';
 import Logo from '../../img/logo.png';
 import User from '../../img/user.jpeg';
 
@@ -107,7 +107,110 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="main-content"></div>
+          <div className="main-content">
+
+            <div className="main-content-header">
+              <div className="header-text">
+                <h1>Overview</h1>
+                <p>On this page you will get an overview of all active stocks in your watchlists.</p>
+              </div>
+              <div className="header-total">
+                <h5>Total Gain</h5>
+                <div className="total-price">
+                  <p>14.7% / 230 391 KR</p> <ArrowUp />
+                </div>
+              </div>
+            </div>
+
+            <hr />
+
+            <div className="main-content-favorites">
+              <h5>Favorites</h5>
+              <div className="favorites">
+
+                <div className="card">
+                  <p className="card-category">Crypto Currency</p>
+                  <p className="card-name">Bitcoin</p>
+                  <div className="card-price positive">
+                    <p>0.72% / 201331.95 KR</p>
+                    <ArrowUp />
+                  </div>
+                </div>
+
+                <div className="card">
+                  <p className="card-category">NasDaq</p>
+                  <p className="card-name">SnesTech, Inc</p>
+                  <div className="card-price negative">
+                    <p>7.272% / 0.12 $</p>
+                    <ArrowUp />
+                  </div>
+                </div>
+
+                <div className="card">
+                  <p className="card-category">Omx</p>
+                  <p className="card-name">Kindred Group</p>
+                  <div className="card-price negative">
+                    <p>-1.75% / 2.55 KR</p>
+                    <ArrowUp />
+                  </div>
+                </div>
+
+                <div className="card">
+                  <p className="card-category">Omx</p>
+                  <p className="card-name">Intrum</p>
+                  <div className="card-price positive">
+                    <p>0.78% / 2.20 KR</p>
+                    <ArrowUp />
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+
+            <hr />
+
+            <div className="main-content-watchlists">
+              <h5>Watchlists</h5>
+              <div className="watchlists">
+
+                <div className="card">
+                  <p className="card-name">My Favorites</p>
+                  <div className="card-price positive">
+                    <p>0.72% / 201331.95 KR</p>
+                    <ArrowUp />
+                  </div>
+                </div>
+
+                <div className="card">
+                  <p className="card-name">My Watchlist</p>
+                  <div className="card-price negative">
+                    <p>7.272% / 0.12 $</p>
+                    <ArrowUp />
+                  </div>
+                </div>
+
+                <div className="card">
+                  <p className="card-name">Magic Formula</p>
+                  <div className="card-price negative">
+                    <p>-1.75% / 2.55 KR</p>
+                    <ArrowUp />
+                  </div>
+                </div>
+
+                <div className="card">
+                  <p className="card-name">Crypto Currencies</p>
+                  <div className="card-price positive">
+                    <p>0.78% / 2.20 KR</p>
+                    <ArrowUp />
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+
+          </div>
         </div>
       </main>
     </>
