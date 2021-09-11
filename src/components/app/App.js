f9cb3db6d3f3b85f0598e5751ed9dc66cde38fcf
Gnusson.net <admin@gnusson.net>
2021-09-11 12:05:28 +0200

    Fixed a bug that broke Routers on production build

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -35,7 +35,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <MemoryRouter>
+      <MemoryRouter basename="moonstocks">
         <Navbar />
         <Base>
           <Switch>
