029ccbdb4ed1705bfbc8ef92b566a00ed8d69552
Gnusson.net <admin@gnusson.net>
2021-09-13 14:49:32 +0200

    Formating

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
 
 app.get('/api', (req, res) => {
-  res.send('Moonstocks BACKEND!!!!!!!!!!')
-})
+  res.send('Moonstocks BACKEND!!!!!!!!!!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
