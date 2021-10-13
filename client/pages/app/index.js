be257d0f06501f97d81f2b1a227f9971bdc4d477
Gnusson.net <admin@gnusson.net>
2021-10-13 16:19:39 +0200

    Deny access if session not exists

--- client/pages/app/index.js
+++ client/pages/app/index.js
@@ -1,11 +1,11 @@
 import Layout from '@components/layouts/Layout';
 
-import styles from '@styles/modules/pages/Test.module.scss'
+import styles from '@styles/modules/pages/Test.module.scss';
 
-export default function Test() {
+export default function Test({ data }) {
   return (
     <section className={styles.page}>
-      <h1>Test</h1>
+      <h1>{data}</h1>
       <h1>Test</h1>
       <h1>Test</h1>
       <h1>Test</h1>
@@ -27,11 +27,27 @@ export default function Test() {
       <h1>Test</h1>
       <h1>Test</h1>
     </section>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const session = 'test';
+  console.log(session);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  const data = 'Logged In';
+
+  return { props: { data } };
 }
 
 Test.getLayout = function getLayout(page) {
-  return (
-    <Layout>{page}</Layout>
-  )
-}
+  return <Layout>{page}</Layout>;
+};
