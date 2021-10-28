f6b2f2ee5525c3f2bceaa9a8df29b293b5f857b5
Gnusson.net <admin@gnusson.net>
2021-10-28 21:03:03 +0200

    Removed SW

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,16 +1,7 @@
 import Head from 'next/head';
-import { useEffect } from 'react';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').catch((err) => console.error('Service worker registration failed', err));
-    } else {
-      console.log('Service worker not supported');
-    }
-  }, []);
-
   return (
     <>
       <Head>
