0b312d19e24b7380942488efbc8a8b1e53f914d5
Gnusson.net <admin@gnusson.net>
2021-10-28 20:41:10 +0200

    service worker

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,14 @@
 import Head from 'next/head';
-import '../styles/globals.css';
+import { useEffect } from 'react';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js');
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -10,7 +17,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#131217" />
         <meta name="msapplication-TileColor" content="#131217" />
         <meta name="theme-color" content="#131217" />
